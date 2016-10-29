d939afabb47f8cc80e8eb30e4f5f2a1cfd998cd9
Facu Obelar Schiera <facu21@hotmail.com>
2016-10-29 14:03:55 -0300

    ejercicio3creoqpronto

--- ejercicio3/Program.cs
+++ ejercicio3/Program.cs
@@ -3,14 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ejercicio3
 {
     class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
-            Console.WriteLine("s");
-        }
+           
+                Console.WriteLine("Ingrese Ruta");
+                 string iRuta = Console.ReadLine();
+                 string mostrar;
+               try
+                {
+                    StreamReader leer = new StreamReader(@iRuta);
+                    while ((mostrar = leer.ReadLine()) != null)
+                    {
+                        Console.WriteLine(mostrar);
+                    }
+                    leer.Close();
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.WriteLine("No se encontro el archivo");
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    Console.WriteLine("No se encontro el directorio");
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("no se ha proporcionado Ruta");
+                }
+                catch (IOException)
+                {
+                    Console.WriteLine("ruta mal escrita");
+                }
+                Console.ReadKey();
+                }
     }
 }
