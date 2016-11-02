3b247edec6c787b6ccc6da81b5cecce313bd68a3
diego <diego@FREDY>
2016-11-02 17:53:31 -0300

    EJ2

---
+++ EJ2.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EJ2;
+
+namespace EJ2.Tests
+{
+    [TestClass]
+    public class UnitMatematica
+    {
+        [TestMethod]
+        public void TestDivisionPorCero()
+        {
+            Matematica mat = new Matematica();
+            
+            int div = 10;
+            int num = 0;
+            double resul = mat.Dividir(div, num);
+            Assert.AreSame(new DivisionPorCeroExcepcion(), resul);
+        }
+    }
+}
---
+++ EJ2/FachadaMatematicas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ2
+{
+    class FachadaMatematicas
+    {
+        Matematica mat = new Matematica();
+
+        public double Dividir(int pDivisor, int pDividendo)
+        {
+            return mat.Dividir(pDivisor, pDividendo);
+        }
+    }
+}
